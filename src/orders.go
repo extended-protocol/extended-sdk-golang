c115717ad117b560e604d11d0dd1931083e90e2d
scaraven <sc4raven@gmail.com>
2025-09-04 13:54:21 +0100

    feat: implement PerpetualOrderModel struct

--- src/orders.go
+++ src/orders.go
@@ -1,29 +1,57 @@
 package sdk
 
-import "math/big"
+// Signature represents a cryptographic signature
+type Signature struct {
+	R string `json:"r"`
+	S string `json:"s"`
+}
+
+// Settlement represents settlement information for an order
+type Settlement struct {
+	Signature          Signature `json:"signature"`
+	StarkKey           string    `json:"starkKey"`
+	CollateralPosition string    `json:"collateralPosition"`
+}
+
+// ConditionalTrigger represents trigger conditions for conditional orders
+type ConditionalTrigger struct {
+	TriggerPrice       string `json:"triggerPrice"`
+	TriggerPriceType   string `json:"triggerPriceType"`   // e.g., "LAST"
+	Direction          string `json:"direction"`          // e.g., "UP", "DOWN"
+	ExecutionPriceType string `json:"executionPriceType"` // e.g., "LIMIT", "MARKET"
+}
+
+// TpSlTrigger represents take profit or stop loss trigger settings
+type TpSlTrigger struct {
+	TriggerPrice     string     `json:"triggerPrice"`
+	TriggerPriceType string     `json:"triggerPriceType"` // e.g., "LAST"
+	Price            string     `json:"price"`
+	PriceType        string     `json:"priceType"` // e.g., "LIMIT"
+	Settlement       Settlement `json:"settlement"`
+}
 
 // PerpetualOrderModel represents a perpetual order
 type PerpetualOrderModel struct {
-	ID                       string      `json:"id"`
-	Market                   string      `json:"market"`
-	Type                     interface{} `json:"type"`  // TODO: Define OrderType enum
-	Side                     interface{} `json:"side"`  // TODO: Define OrderSide enum
-	Qty                      *big.Float  `json:"qty"`   // Using big.Float for Decimal
-	Price                    *big.Float  `json:"price"` // Using big.Float for Decimal
-	ReduceOnly               bool        `json:"reduce_only"`
-	PostOnly                 bool        `json:"post_only"`
-	TimeInForce              interface{} `json:"time_in_force"` // TODO: Define TimeInForce enum
-	ExpiryEpochMillis        int64       `json:"expiry_epoch_millis"`
-	Fee                      *big.Float  `json:"fee"`                         // Using big.Float for Decimal
-	Nonce                    *big.Float  `json:"nonce"`                       // Using big.Float for Decimal
-	SelfTradeProtectionLevel interface{} `json:"self_trade_protection_level"` // TODO: Define SelfTradeProtectionLevel enum
-	CancelID                 *string     `json:"cancel_id,omitempty"`         // Optional field
-	Settlement               interface{} `json:"settlement,omitempty"`        // TODO: Define StarkSettlementModel
-	Trigger                  interface{} `json:"trigger,omitempty"`           // TODO: Define CreateOrderConditionalTriggerModel
-	TpSlType                 interface{} `json:"tp_sl_type,omitempty"`        // TODO: Define OrderTpslType enum
-	TakeProfit               interface{} `json:"take_profit,omitempty"`       // TODO: Define CreateOrderTpslTriggerModel
-	StopLoss                 interface{} `json:"stop_loss,omitempty"`         // TODO: Define CreateOrderTpslTriggerModel
-	DebuggingAmounts         interface{} `json:"debugging_amounts,omitempty"` // TODO: Define StarkDebuggingOrderAmountsModel
-	BuilderFee               *big.Float  `json:"builderFee,omitempty"`        // Optional, using big.Float for Decimal
-	BuilderID                *int        `json:"builderId,omitempty"`         // Optional field
+	ID                       string              `json:"id"`
+	Market                   string              `json:"market"`
+	Type                     string              `json:"type"` // e.g., "CONDITIONAL", "LIMIT", "MARKET"
+	Side                     string              `json:"side"` // e.g., "BUY", "SELL"
+	Qty                      string              `json:"qty"`
+	Price                    string              `json:"price"`
+	TimeInForce              string              `json:"timeInForce"` // e.g., "GTT", "GTC", "IOC"
+	ExpiryEpochMillis        int64               `json:"expiryEpochMillis"`
+	Fee                      string              `json:"fee"`
+	Nonce                    string              `json:"nonce"`
+	Settlement               Settlement          `json:"settlement"`
+	ReduceOnly               bool                `json:"reduceOnly"`
+	PostOnly                 bool                `json:"postOnly"`
+	SelfTradeProtectionLevel string              `json:"selfTradeProtectionLevel"`    // e.g., "ACCOUNT"
+	Trigger                  *ConditionalTrigger `json:"trigger,omitempty"`           // Optional for conditional orders
+	TpSlType                 *string             `json:"tpSlType,omitempty"`          // e.g., "ORDER"
+	TakeProfit               *TpSlTrigger        `json:"takeProfit,omitempty"`        // Optional take profit settings
+	StopLoss                 *TpSlTrigger        `json:"stopLoss,omitempty"`          // Optional stop loss settings
+	BuilderFee               *string             `json:"builderFee,omitempty"`        // Optional builder fee
+	BuilderID                *int                `json:"builderId,omitempty"`         // Optional builder ID
+	CancelID                 *string             `json:"cancel_id,omitempty"`         // Optional field
+	DebuggingAmounts         interface{}         `json:"debugging_amounts,omitempty"` // TODO: Define StarkDebuggingOrderAmountsModel
 }
