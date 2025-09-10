dde8883c8079a35eeb2ef2bea87e487ba29c6e54
scaraven <sc4raven@gmail.com>
2025-09-10 10:20:03 +0100

    feat: rename json ser to camel case

--- src/markets.go
+++ src/markets.go
@@ -2,18 +2,18 @@ package sdk
 
 type L2ConfigModel struct {
 	Type                 string `json:"type"`
-	CollateralID         string `json:"collateral_id"`
-	CollateralResolution int64  `json:"collateral_resolution"`
-	SyntheticID          string `json:"synthetic_id"`
-	SyntheticResolution  int64  `json:"synthetic_resolution"`
+	CollateralID         string `json:"collateralId"`
+	CollateralResolution int64  `json:"collateralResolution"`
+	SyntheticID          string `json:"syntheticId"`
+	SyntheticResolution  int64  `json:"syntheticResolution"`
 }
 
 type MarketModel struct {
 	Name                     string        `json:"name"`
-	AssetName                string        `json:"asset_name"`
-	AssetPrecision           int           `json:"asset_precision"`
-	CollateralAssetName      string        `json:"collateral_asset_name"`
-	CollateralAssetPrecision int           `json:"collateral_asset_precision"`
+	AssetName                string        `json:"assetName"`
+	AssetPrecision           int           `json:"assetPrecision"`
+	CollateralAssetName      string        `json:"collateralAssetName"`
+	CollateralAssetPrecision int           `json:"collateralAssetPrecision"`
 	Active                   bool          `json:"active"`
-	L2Config                 L2ConfigModel `json:"l2_config"`
+	L2Config                 L2ConfigModel `json:"l2Config"`
 }
